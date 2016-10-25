6c595a75dc2288a305f87fd1808a75fecbbc1c23
Sean Collins <sean0000@macbookbro.local>
2016-10-25 15:11:46 -0700

    Basic view all and create functionality

---
+++ public/app.js
@@ -0,0 +1,100 @@
+angular.module('RedisGUI', [])
+    .controller('MainCtrl', MainCtrl)
+    .service('$RedisServices', ServiceModule);
+
+function ServiceModule($http) {
+    var service = this;
+
+    // Create new key
+    service.createKey = function(keyData) {
+        return $http({
+            url: '/api/keys/create',
+            method: 'POST',
+            dataType: "json",
+            async: "true",
+            data: JSON.stringify({
+                keyId: keyData.keyId,
+                keyValue: keyData.keyValue
+            }),
+            headers: {
+                "Accept": "application/json"
+            }
+        });
+    };
+
+    // Delete key
+    service.deleteKey = function(keyId) {
+        return $http({
+            url: '/api/keys/delete/' + keyId,
+            method: 'GET',
+            dataType: "json",
+            async: "true",
+            headers: {
+                "Accept": "application/json"
+            }
+        });
+    };
+
+    // Load all keys
+    service.loadKeys = function() {
+        return $http({
+            url: '/api/keys/list',
+            method: 'GET',
+            dataType: "json",
+            async: "true",
+            headers: {
+                "Accept": "application/json"
+            }
+        });
+    };
+}
+
+function MainCtrl($scope, $RedisServices) {
+    $scope.currentKey = {};
+    $scope.keys = [];
+    $scope.loading = false;
+    $scope.newKey = {};
+
+    function init() {
+        $scope.loadKeys();
+    };
+
+    // Create key
+    $scope.createKey = function(keyData) {
+        $scope.loading = true;
+        var createKey = $RedisServices.createKey(keyData);
+        createKey.then(function() {
+            var loadKeys = $RedisServices.loadKeys();
+            loadKeys.then(function(response) {
+                $scope.keys = response.data;
+                $scope.newKey = {};
+                $scope.loading = false;
+            });
+        });
+    };
+
+    // Delete key
+    $scope.deleteKey = function(keyId) {
+        $scope.loading = true;
+        var deleteKey = $RedisServices.deleteKey(keyId);
+        deleteKey.then(function() {
+            var loadKeys = $RedisServices.loadKeys();
+            loadKeys.then(function(response) {
+                $scope.keys = response.data;
+                $scope.loading = false;
+            });
+        });
+    };
+
+    // Load all keys
+    $scope.loadKeys = function() {
+        $scope.loading = true;
+        var loadKeys = $RedisServices.loadKeys();
+        loadKeys.then(function(response) {
+            $scope.keys = response.data;
+            $scope.loading = false;
+        });
+    };
+
+    init();
+}
---
+++ server.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// Dependencies
+var express         = require('express'),
+    app             = express(),
+    cookieParser    = require('cookie-parser'),
+    bodyParser      = require('body-parser'),
+    methodOverride  = require('method-override'),
+    redis           = require('redis'),
+    client          = redis.createClient({host: 'localhost', port: 6379}),
+    port            = 3000;
+
+//  Server config
+app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(cookieParser());
+app.use(methodOverride());
+
+// Redis config
+client.on('error', function(err) {
+    console.log('Error: ' + err);
+});
+app.get('/api/keys/list', function(req, res) {
+    client.keys('*', function(err, reply) {
+        res.send(reply);
+    });
+});
+app.post('/api/keys/create', function(req, res) {
+    client.set(req.body.keyId, req.body.keyValue, redis.print);
+});
+app.get('/api/keys/delete/:keyId', function(req, res) {
+    client.del(req.params.keyId, function(err, reply) {
+        res.send(reply);
+    });
+});
+
+// Startuo
+app.listen(port, function() {
+    console.log('Listening on port ' + port + '...');
+});
