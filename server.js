4a078cca88bd2734f17c66054bd7e17931fbbf63
thehouseplant <sean.collins@outlook.com>
2021-12-31 16:58:09 -0800

    Whitespacing and variable declaration updates

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 'use strict';
 
 // Dependencies
-var express         = require('express'),
-    app             = express(),
-    cookieParser    = require('cookie-parser'),
-    bodyParser      = require('body-parser'),
-    methodOverride  = require('method-override'),
-    redis           = require('redis'),
-    client          = redis.createClient({host: 'localhost', port: 6379}),
-    port            = 3000;
+let express         = require('express');
+let app             = express();
+let cookieParser    = require('cookie-parser');
+let bodyParser      = require('body-parser');
+let methodOverride  = require('method-override');
+let redis           = require('redis');
+let client          = redis.createClient({host: 'localhost', port: 6379});
+let port            = 3000;
 
-//  Server config
+
+// Server configuration
 app.use(express.static(__dirname + '/public'));
 app.use('/public', express.static(__dirname + '/public'));
 app.use(bodyParser.json());
@@ -20,32 +21,38 @@ app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(cookieParser());
 app.use(methodOverride());
 
-// Redis config
+
+// Redis configuration
 client.on('error', function(err) {
     console.log('Error: ' + err);
 });
+
 app.get('/api/keys/list', function(req, res) {
     client.keys('*', function(err, reply) {
         res.send(reply);
     });
 });
+
 app.get('/api/keys/get/:keyId', function(req, res) {
     client.get(req.params.keyId, function(err, reply) {
         res.send(reply);
     });
 });
+
 app.post('/api/keys/create', function(req, res) {
     client.set(req.body.keyId, req.body.keyValue, function(err, reply) {
         res.send('Key created');
     });
 });
+
 app.get('/api/keys/delete/:keyId', function(req, res) {
     client.del(req.params.keyId, function(err, reply) {
         res.send('Key ' + req.params.keyId + ' deleted');
     });
 });
 
-// Startuo
+
+// Application start
 app.listen(port, function() {
     console.log('Listening on port ' + port + '...');
 });
